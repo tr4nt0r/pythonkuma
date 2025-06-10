b544edb43231d58f55ac363b825667f1b0b3b548
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-06-10 17:26:12 +0200

    Make explicit imports and add `__all__` to `__init__.py`

--- pyuptimekuma/__init__.py
+++ pyuptimekuma/__init__.py
@@ -1,7 +1,20 @@
 """Python API wrapper for Uptime Kuma."""
-from .exceptions import *
-from .models import *
+from .exceptions import (
+    UptimeKumaAuthenticationException,
+    UptimeKumaConnectionException,
+    UptimeKumaException,
+)
+from .models import MonitorType, UptimeKumaApiResponse, UptimeKumaMonitor
 from .uptimekuma import UptimeKuma
 
 __version__ = '0.0.0'
 
+__all__ = [
+    "MonitorType",
+    "UptimeKuma",
+    "UptimeKumaApiResponse",
+    "UptimeKumaAuthenticationException",
+    "UptimeKumaConnectionException",
+    "UptimeKumaException",
+    "UptimeKumaMonitor",
+]
