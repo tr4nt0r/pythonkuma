8c4755173062e181e381284c4583dc7f0b715838
Manu <4445816+tr4nt0r@users.noreply.github.com>
2025-06-10 22:40:50 +0200

    Add MonitorStatus to exports (#31)

--- pythonkuma/__init__.py
+++ pythonkuma/__init__.py
@@ -1,16 +1,13 @@
 """Python API wrapper for Uptime Kuma."""
 
-from .exceptions import (
-    UptimeKumaAuthenticationException,
-    UptimeKumaConnectionException,
-    UptimeKumaException,
-)
-from .models import MonitorType, UptimeKumaApiResponse, UptimeKumaMonitor
+from .exceptions import UptimeKumaAuthenticationException, UptimeKumaConnectionException, UptimeKumaException
+from .models import MonitorStatus, MonitorType, UptimeKumaApiResponse, UptimeKumaMonitor
 from .uptimekuma import UptimeKuma
 
 __version__ = "0.0.0rc0"
 
 __all__ = [
+    "MonitorStatus",
     "MonitorType",
     "UptimeKuma",
     "UptimeKumaApiResponse",
