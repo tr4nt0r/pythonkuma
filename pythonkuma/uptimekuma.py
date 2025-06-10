3614bdb66cf581adbb5b98b47bb92e7eb10b2db5
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-06-10 21:23:59 +0200

    Use yarl for URL building

--- pythonkuma/uptimekuma.py
+++ pythonkuma/uptimekuma.py
@@ -1,6 +1,7 @@
 """Uptime Kuma client."""
 
 from aiohttp import ClientSession
+from yarl import URL
 
 from .decorator import api_request
 from .models import UptimeKumaApiResponse
@@ -9,14 +10,14 @@ from .models import UptimeKumaApiResponse
 class UptimeKuma:
     """This class is used to get information from Uptime Kuma."""
 
-    def __init__(self, session: ClientSession, base_url: str, username: str, password: str) -> None:
+    def __init__(self, session: ClientSession, base_url: URL | str, username: str, password: str) -> None:
         """Initialize"""
         self.monitors = []
-        self._base_url = base_url
+        self._base_url = base_url if isinstance(base_url, URL) else URL(base_url)
         self._username = username
         self._password = password
         self._session: ClientSession = session
 
-    @api_request("/metrics")
+    @api_request("metrics")
     async def async_get_monitors(self, **kwargs) -> UptimeKumaApiResponse:
         """Get monitors from API."""
