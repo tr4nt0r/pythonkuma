563bc5c0fdf84f1f4902a369ed6fd3b6297657ae
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-07-06 04:09:56 +0200

    Bump version 0.2.0 → 0.3.0

--- pythonkuma/__init__.py
+++ pythonkuma/__init__.py
@@ -10,7 +10,7 @@ from .models import MonitorStatus, MonitorType, UptimeKumaMonitor, UptimeKumaVer
 from .update import UpdateChecker
 from .uptimekuma import UptimeKuma
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "MonitorStatus",
