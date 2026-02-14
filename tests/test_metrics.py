3bdd0d4bfd8e86baa512277328c992ab2dce0df5
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2026-02-14 12:05:35 +0100

    Add unit tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,10 +1,19 @@
 """Tests for pythonkuma."""
 
-from unittest.mock import AsyncMock
+from http import HTTPStatus
+from typing import Any
+from unittest.mock import AsyncMock, Mock
 
+from aiohttp import ClientError, ClientResponseError, ConnectionTimeoutError
+import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from pythonkuma import UptimeKuma
+from pythonkuma.exceptions import (
+    UptimeKumaAuthenticationException,
+    UptimeKumaConnectionException,
+    UptimeKumaParseException,
+)
 
 
 async def test_metrics(mock_session: AsyncMock, snapshot: SnapshotAssertion) -> None:
@@ -14,3 +23,62 @@ async def test_metrics(mock_session: AsyncMock, snapshot: SnapshotAssertion) -> 
     response = await uptime_kuma.metrics()
 
     assert {k: v.to_dict() for k, v in response.items()} == snapshot
+
+    assert uptime_kuma.version.version == "2.1.0"
+    assert uptime_kuma.version.major == "2"
+    assert uptime_kuma.version.minor == "1"
+    assert uptime_kuma.version.patch == "0"
+
+
+@pytest.mark.parametrize(
+    ("exception", "expected_exception", "error_msg"),
+    [
+        (
+            ClientResponseError(
+                request_info=Mock(), history=(Mock()), status=HTTPStatus.NOT_FOUND
+            ),
+            UptimeKumaConnectionException,
+            (
+                "Request for %s failed with status code %s",
+                "http://uptime.example.com/metrics",
+                HTTPStatus.NOT_FOUND,
+            ),
+        ),
+        (
+            ClientResponseError(
+                request_info=Mock(), history=(Mock()), status=HTTPStatus.UNAUTHORIZED
+            ),
+            UptimeKumaAuthenticationException,
+            ("Authentication failed for %s", "http://uptime.example.com/metrics"),
+        ),
+        (ClientError, UptimeKumaConnectionException, ()),
+        (
+            ConnectionTimeoutError,
+            UptimeKumaConnectionException,
+            ("Request timeout for %s", "http://uptime.example.com/metrics"),
+        ),
+    ],
+)
+async def test_exceptions(
+    mock_session: AsyncMock,
+    exception: Exception,
+    expected_exception: Exception,
+    error_msg: tuple[Any],
+) -> None:
+    """Test request exceptions."""
+    mock_session.get.side_effect = exception
+    uptime_kuma = UptimeKuma(mock_session, "http://uptime.example.com", "test-apikey")
+
+    with pytest.raises(expected_exception) as e:
+        await uptime_kuma.metrics()
+
+    assert e.value.args == error_msg
+
+
+async def test_metrics_parse_exceptions(mock_session: AsyncMock) -> None:
+    """Test prometheus metrics parsing fails."""
+    mock_session.get.return_value.text.return_value = "invalid metrics"
+    uptime_kuma = UptimeKuma(mock_session, "http://uptime.example.com", "test-apikey")
+
+    with pytest.raises(UptimeKumaParseException):
+        await uptime_kuma.metrics()
